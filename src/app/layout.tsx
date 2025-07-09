b6592d6275abac718e703354d4ed488e0387bff1
Daniele Pishinin::nn <danipishinin@gmail.com>
2025-07-09 13:33:27 -0300

    fix: update copyright

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,16 +41,12 @@ export default function RootLayout({
         </main>
         <footer className="mx-auto flex max-w-prose flex-col items-center gap-2 py-6 text-sm text-zinc-500 dark:text-zinc-400">
           <div className="flex items-center gap-4">
-            <p className="decoration-zinc-500 underline-offset-4 transition-all sm:hover:underline dark:decoration-zinc-400">
-              by LukeberryPi
-            </p>
-
             <a
               className="decoration-zinc-500 underline-offset-4 transition-all sm:hover:underline dark:decoration-zinc-400"
-              href="https://x.com/danipishinin"
+              href="https://github.com/LukeberryPi/blog"
               target="_blank"
             >
-              @danipishinin
+              ©
             </a>
           </div>
           <blockquote className="text-zinc-800 dark:text-zinc-300">
